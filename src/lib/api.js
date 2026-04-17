e87fe2ed3924d0306b7db646175980a785c1e2c1
sivanesan1103 <sivanesank1103@gmail.com>
2026-04-17 19:23:26 +0530

    s

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+const configuredApiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const normalizedApiBaseUrl = configuredApiBaseUrl
+  ? configuredApiBaseUrl.replace(/\/+$/, "")
+  : "http://localhost:8080";
+const API_BASE_URL = normalizedApiBaseUrl.endsWith("/api")
+  ? normalizedApiBaseUrl
+  : `${normalizedApiBaseUrl}/api`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
