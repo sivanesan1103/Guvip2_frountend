d24666ddc4ac9f0159136027754ad363d109bb28
sivanesan1103 <sivanesank1103@gmail.com>
2026-04-17 17:54:51 +0530

    email updated

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
